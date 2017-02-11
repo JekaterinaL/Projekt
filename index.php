ba5ffee56a44262392c1cbc6aae06b8a612ea3d5
JekaterinaL <jekaterina.losseva@itcollege.ee>
2017-02-11 16:15:19 +0200

    Praktika

---
+++ index.php
@@ -0,0 +1,30 @@
+﻿<?php
+// prints e.g. 'Current PHP version: 4.1.1'
+echo 'Current PHP version: ' . phpversion();
+
+// prints e.g. '2.0' or nothing if the extension isn't enabled
+echo phpversion('tidy');
+
+ $host = "localhost";
+    $user = "test";
+    $pass = "t3st3r123";
+    $db = "test";
+
+    $l = mysqli_connect($host, $user, $pass, $db);
+    mysqli_query($l, "SET CHARACTER SET UTF8") or
+            die("Error, ei saa andmebaasi charsetti seatud");
+    mysqli_close($l);
+?>
+<html>
+<body>
+
+<h1>My first heading</h1>
+
+<p>My first paragraph.</p>
+
+<img src="picture.jpg" width="142" height="104">
+
+<link rel="stylesheet" href="styles.css">
+
+</body>
+</html>
