7f1055f65dd3e675df4846d285672a2be5fb20e8
JekaterinaL <jekaterina.losseva@itcollege.ee>
2017-03-04 16:02:00 +0200

    Praktika2

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 ﻿<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // prints e.g. 'Current PHP version: 4.1.1'
 echo 'Current PHP version: ' . phpversion();
 
 // prints e.g. '2.0' or nothing if the extension isn't enabled
 echo phpversion('tidy');
 
- $host = "localhost";
+ 	$host = "localhost";
     $user = "test";
     $pass = "t3st3r123";
+    $db = "test";
     $db = "test";
 
     $l = mysqli_connect($host, $user, $pass, $db);
@@ -16,7 +20,12 @@ echo phpversion('tidy');
     mysqli_close($l);
 ?>
 <html>
-<body>
+</head>
+
+<body onload="startTime()">
+<script src="clock.js"></script>
+
+<div id="txt"></div>
 
 <h1>My first heading</h1>
 
@@ -24,7 +33,10 @@ echo phpversion('tidy');
 
 <img src="picture.jpg" width="142" height="104">
 
-<link rel="stylesheet" href="styles.css">
+<p><link rel="stylesheet" href="styles.css"></p>
+ <a href="http://jigsaw.w3.org/css-validator/check/referer">
+  <img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
+ </a>
 
 </body>
 </html>
