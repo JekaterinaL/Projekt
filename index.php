248827e02edd52ebf971fc246eb4f93dc1272845
JekaterinaL <jekaterina.losseva@itcollege.ee>
2017-03-05 18:31:12 +0200

    Ylesanne3

--- index.php
+++ index.php
@@ -6,18 +6,62 @@ ini_set('display_errors', 1);
 echo 'Current PHP version: ' . phpversion();
 
 // prints e.g. '2.0' or nothing if the extension isn't enabled
-echo phpversion('tidy');
+echo phpversion();
 
  	$host = "localhost";
     $user = "test";
     $pass = "t3st3r123";
     $db = "test";
-    $db = "test";
+    $table = "10163487_table";
 
     $l = mysqli_connect($host, $user, $pass, $db);
     mysqli_query($l, "SET CHARACTER SET UTF8") or
             die("Error, ei saa andmebaasi charsetti seatud");
     mysqli_close($l);
+?>
+<?php
+$db_host = '*****';
+$db_user = '*****';
+$db_pwd = '*****';
+
+$database = '*****';
+$table = 'Guitars';
+
+if (!mysql_connect($db_host, $db_user, $db_pwd))
+    die("Can't connect to database");
+
+if (!mysql_select_db($database))
+    die("Can't select database");
+
+// sending query
+$result = mysql_query("SELECT * FROM {$table}");
+if (!$result) {
+    die("Query to show fields from table failed");
+} 
+
+$fields_num = mysql_num_fields($result);
+
+echo "<table border='1'><tr>";
+// printing table headers
+for($i=0; $i<$fields_num; $i++)
+{
+    $field = mysql_fetch_field($result);
+    echo "<td>{$field->name}</td>";
+}
+echo "</tr>\n";
+// printing table rows
+while($row = mysql_fetch_row($result))
+{
+    echo "<tr>";
+
+    // $row is array... foreach( .. ) puts every element
+    // of $row to $cell variable
+     foreach($row as $cell)
+        echo "<td>$cell</td>";
+
+    echo "</tr>\n";
+}
+mysql_free_result($result);
 ?>
 <html>
 </head>
