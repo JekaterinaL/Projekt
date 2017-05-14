a4ae5325190a99b058f027cee902409e005c3022
JekaterinaL <jekaterina.losseva@itcollege.ee>
2017-05-14 23:02:05 +0300

    Ylesanne13

--- funk.php
+++ funk.php
@@ -11,9 +11,62 @@ function connect_db(){
 	mysqli_query($connection, "SET CHARACTER SET UTF8") or die("Ei saanud baasi utf-8-sse - ".mysqli_error($connection));
 }
 
-function logi(){
-	// siia on vaja funktsionaalsust (13. nädalal)
 
+function kuva_puurid(){
+	// siia on vaja funktsionaalsust
+	global $connection;
+	if (!empty($_SESSION['user'])) {
+		$p = mysqli_query($connection, "select distinct(puur) as puur from 10163487_loomaaed order by puur asc");
+		$puurid = array();
+		while ($r = mysqli_fetch_assoc($p)){
+			$l = mysqli_query($connection, "SELECT * FROM 10163487_loomaaed WHERE  puur=".mysqli_real_escape_string($connection, $r['puur']));
+			while ($row = mysqli_fetch_assoc($l)) {
+				$puurid[$r['puur']][] = $row;
+			}
+		}
+		include_once('views/puurid.html');
+	} else {
+		include_once('views/login.html');
+	}
+}
+
+function logi(){
+	// INSERT INTO 10163487_kylastajad (username, passw) VALUES('Jekaterina', SHA1('katja'))
+	// siia on vaja funktsionaalsust (13. nädalal)
+	
+	//if ($_SERVER['REQUEST_METHOD'] == 'POST'){}
+	global $connection;
+	if (!empty($_SESSION['user'])) {
+		header("Location: ?page=loomad");
+	} else {
+		if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+			if(!empty($_POST)){
+				if ($_POST['user'] != "" && $_POST['pass'] != "") {
+					$user = mysqli_real_escape_string($connection, $_POST['user']);
+					$pass = mysqli_real_escape_string($connection, $_POST['pass']);
+					
+					$sql = "SELECT id, username FROM 10163487_kylastajad WHERE username = '$user' AND passw = SHA1('$pass')";
+					$result = mysqli_query($connection, $sql);
+					if ($result && $user = mysqli_fetch_assoc($result)){
+					// if (mysqli_num_rows($result)) { - eelmise rea asemele
+						$_SESSION['user'] = $_POST['user'];
+						header("Location: ?page=loomad");
+						// exit(0);
+					} else {
+						$errors = array();
+						$errors[] = "Login luhtus, kas oli õige info?";
+					} 
+				} else {
+					if (empty($_POST['user'])){
+						$errors[] = "Kasutajanimi vajalik!";
+					}
+					if (empty($_POST['pass'])){
+						$errors[] = "Parool vajalik!";
+					}
+				}
+			}
+		}
+	}
 	include_once('views/login.html');
 }
 
@@ -23,37 +76,37 @@ function logout(){
 	header("Location: ?");
 }
 
-function kuva_puurid(){
-	// siia on vaja funktsionaalsust
-	global $connection;
-	$puurid = array();
-	$query = "SELECT DISTINCT puur FROM 10163487_loomaaed";
-	$result = mysqli_query($connection, $query) or die("$query - ".mysqli_error($connection));
-	// hangime tulemused
-	while ($puuri_nr = mysqli_fetch_assoc($result)){
-		//echo "{$puuri_nr['puur']} <br/>";
-		$query2 = "SELECT * FROM 10163487_loomaaed WHERE puur = ".mysqli_real_escape_string($connection, $puuri_nr['puur']);
-		$result2 = mysqli_query($connection, $query2) or die ("$query2 - ".mysqli_error($connection));
-		while ($loomarida = mysqli_fetch_assoc($result2)){
-			$puurid[$puuri_nr['puur']][] = $loomarida;
-			/*echo "<p><pre>";
-			print_r($puurid);
-			echo "</pre>";*/
-		}
-		/*echo "<p><pre>";
-		print_r($puurid);
-		echo "</pre>";*/
-	}
-	/*echo "<p><pre>";
-	print_r($puurid);
-	echo "</pre>";*/
-	include_once('views/puurid.html');
-	
-}
-
 function lisa(){
 	// siia on vaja funktsionaalsust (13. nädalal)
 	
+	global $connection;
+	
+	if (empty($_SESSION['user'])) {
+		header("Location: ?page=login");
+	} else {
+		if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+			if ($_POST['nimi'] == "") {
+				$errors = array();
+				$errors[] = "Nimi vajalik!";
+			} elseif ($_POST['puur'] == ""){
+				$errors[] = "Puur vajalik!";
+			} elseif (upload("liik") == ""){
+				$errors[] = "Liik vajalik!";
+			} else {
+				upload('liik');
+				$nimi = mysqli_real_escape_string($connection, $_POST['nimi']);
+				$puur = mysqli_real_escape_string($connection, $_POST['puur']);
+				$liik = mysqli_real_escape_string($connection, substr($_FILES['liik']['name'], 0, -4));
+				$sql = "INSERT INTO 10163487_loomaaed(id, nimi, puur, liik) VALUES (NULL, '$nimi', '$puur', '$liik')";
+				$result = mysqli_query($connection, $sql);
+				
+				if (mysqli_insert_id($connection)) {
+					header("Location: ?page=loomad");
+				}
+			}
+		}
+	}
+
 	include_once('views/loomavorm.html');
 	
 }
